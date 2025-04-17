c7cb7e44612218d381d51e5ed625566936410c00
hama <wowlsh93@gmail.com>
2025-04-17 16:34:19 +0900

    Parallelize API calls using tokio::join!

--- src/routes/question.rs
+++ src/routes/question.rs
@@ -7,6 +7,7 @@ use crate::model::question::{NewQuestion, Question, QuestionId};
 use crate::profanity::check_profanity;
 use tracing::{info, instrument};
 use handle_errors::Error;
+use tokio::join;
 
 #[instrument]
 pub async fn get_questions(
@@ -27,33 +28,29 @@ pub async fn get_questions(
     }
 }
 
-
 pub async fn add_question(
     store: Store,
     new_question: NewQuestion,
 ) -> Result<impl warp::Reply, warp::Rejection> {
-    let title = match check_profanity(new_question.title).await {
-        Ok(res) => res,
-        Err(e) => return Err(warp::reject::custom(e)),
-    };
+    let (title_res, content_res) = join!(
+        check_profanity(new_question.title.clone()),
+        check_profanity(new_question.content.clone())
+    );
     
-    let content = match check_profanity(new_question.content).await {
-        Ok(res) => res,
-        Err(e) => return Err(warp::reject::custom(e)),
-    };
+    let title = title_res.map_err(warp::reject::custom)?;
+    let content = content_res.map_err(warp::reject::custom)?;
     
     let question = NewQuestion {
-        title: title,
+        title,
         content,
         tags: new_question.tags,
     };
     
-    match store.add_question(question).await {
-        Ok(question) => Ok(warp::reply::json(&question)),
-        Err(e) => Err(warp::reject::custom(e)),
-    }
+    let saved = store.add_question(question).await
+      .map_err(warp::reject::custom)?;
+    
+    Ok(warp::reply::json(&saved))
 }
-
 
 pub async fn update_question(
     id: String,
